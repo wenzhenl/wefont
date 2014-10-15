dcfc4ade136f8e46517d64a5c776c36188882de7
Wenzheng Li <wenzhenl@usc.edu>
2014-10-14 23:57:36 -0700

    split img into each char, and save qrcode

---
+++ preprocess/separate_img_into_characters.py
@@ -0,0 +1,249 @@
+#!/usr/bin/python
+
+from PIL import Image
+from PIL import ImageEnhance
+import sys, os
+
+f, e = os.path.splitext(sys.argv[1])
+im = Image.open(sys.argv[1])
+pix = im.load()
+print im.size
+
+if len(sys.argv) > 2:
+    isDebug = sys.argv[1]
+else:
+    isDebug = "notDebug"
+
+ratio = 0.9 
+thres = 130 # the threshold of grey level for white
+
+
+# transfer a graph to its grey version
+total = 0
+for y in range(im.size[1]):
+    for x in range(im.size[0]):
+        g = (pix[x,y][0] + pix[x,y][1] + pix[x,y][2])/3
+        total = total + g
+        pix[x,y] = (g,g,g)
+
+print 'ave grey level', total/(im.size[0] * im.size[1])
+
+white_rows = []
+for y in range(im.size[1]):
+    count = 0
+    for x in range(im.size[0]):
+        if(pix[x,y][0] < thres):
+            break;
+        else:
+            count = count + 1
+    if(count > ratio * im.size[0]):
+        white_rows.append(y)
+
+white_cols = []
+for x in range(im.size[0]):
+    count = 0
+    for y in range(im.size[1]):
+        if(pix[x,y][0] < thres):
+            break;
+        else:
+            count = count + 1
+    if(count > ratio * im.size[1]):
+        white_cols.append(x)
+
+white_bar_cols = []
+start = white_cols[0]
+for x in range(len(white_cols)-1):
+    if white_cols[x+1] - white_cols[x] > 1:
+        white_bar_cols.append((start, white_cols[x], white_cols[x]-start+1))
+        start = white_cols[x+1]
+last_col = white_cols[len(white_cols)-1]
+white_bar_cols.append((start, last_col, last_col-start+1))
+
+white_bar_rows = []
+start = white_rows[0]
+for x in range(len(white_rows)-1):
+    if white_rows[x+1] - white_rows[x] > 1:
+        white_bar_rows.append((start, white_rows[x], white_rows[x]-start+1))
+        start = white_rows[x+1]
+last_row = white_rows[len(white_rows)-1]
+white_bar_rows.append((start, last_row, last_row-start+1))
+
+print '*****************row statistics***************'
+for x in white_bar_rows:
+    print x
+
+print '*****************column statistics***************'
+for x in white_bar_cols:
+    print x
+
+if(isDebug == "debug"):
+    for wy in white_rows:
+        for x in range(im.size[0]):
+            pix[x,wy] = (255, 102 ,102)
+
+    for wx in white_cols:
+        for y in range(im.size[1]):
+            pix[wx, y] = (102, 255, 255)
+    im.show()
+
+# two standard for black bars
+# the white bar length > white_len = 8
+# the black bar length > black_len = 30
+
+white_len = 8
+black_len = 30
+#  select those valid white row bar
+valid_white_bar_rows = []
+for x in white_bar_rows:
+    if(x[2] > white_len): # length of white bar 
+        valid_white_bar_rows.append(x)
+
+black_rows = []
+for x in range(len(valid_white_bar_rows)-1):
+    if(valid_white_bar_rows[x+1][0] - valid_white_bar_rows[x][1] > black_len):
+        black_rows.append((valid_white_bar_rows[x][1]+1, valid_white_bar_rows[x+1][0]-1))
+
+#  select those valid white col bar
+valid_white_bar_cols = []
+for x in white_bar_cols:
+    if(x[2] > white_len): # length of white bar 
+        valid_white_bar_cols.append(x)
+
+black_cols = []
+for x in range(len(valid_white_bar_cols)-1):
+    if(valid_white_bar_cols[x+1][0] - valid_white_bar_cols[x][1] > black_len):
+        black_cols.append((valid_white_bar_cols[x][1]+1, valid_white_bar_cols[x+1][0]-1))
+
+print '*****************row cooridinates***************'
+for x in black_rows:
+    print x
+
+print '*****************col cooridinates***************'
+for x in black_cols:
+    print x
+
+# save the qrcode to png
+box = (black_cols[-1][0], black_rows[0][0], black_cols[-1][1], black_rows[0][1])
+region = im.crop(box)
+region.save('%s_qrcode.png' %f, 'png')
+
+# remove the qrcode img
+for x in range(black_cols[-1][0], black_cols[-1][1]+1):
+    for y in range(black_rows[0][0], black_rows[0][1]+1):
+        pix[x,y] = (255, 255, 255)
+
+im.show()
+
+# /////////////////////////////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////////////////////////
+# reprocess the img after removing qrcode img
+
+
+white_rows = []
+for y in range(im.size[1]):
+    count = 0
+    for x in range(im.size[0]):
+        if(pix[x,y][0] < thres):
+            break;
+        else:
+            count = count + 1
+    if(count > ratio * im.size[0]):
+        white_rows.append(y)
+
+white_cols = []
+for x in range(im.size[0]):
+    count = 0
+    for y in range(im.size[1]):
+        if(pix[x,y][0] < thres):
+            break;
+        else:
+            count = count + 1
+    if(count > ratio * im.size[1]):
+        white_cols.append(x)
+
+white_bar_cols = []
+start = white_cols[0]
+for x in range(len(white_cols)-1):
+    if white_cols[x+1] - white_cols[x] > 1:
+        white_bar_cols.append((start, white_cols[x], white_cols[x]-start+1))
+        start = white_cols[x+1]
+last_col = white_cols[len(white_cols)-1]
+white_bar_cols.append((start, last_col, last_col-start+1))
+
+white_bar_rows = []
+start = white_rows[0]
+for x in range(len(white_rows)-1):
+    if white_rows[x+1] - white_rows[x] > 1:
+        white_bar_rows.append((start, white_rows[x], white_rows[x]-start+1))
+        start = white_rows[x+1]
+last_row = white_rows[len(white_rows)-1]
+white_bar_rows.append((start, last_row, last_row-start+1))
+
+print '*****************SECOND TIME row statistics***************'
+for x in white_bar_rows:
+    print x
+
+print '*****************SECOND TIME column statistics***************'
+for x in white_bar_cols:
+    print x
+
+if(isDebug == "debug"):
+    for wy in white_rows:
+        for x in range(im.size[0]):
+            pix[x,wy] = (255, 102 ,102)
+
+    for wx in white_cols:
+        for y in range(im.size[1]):
+            pix[wx, y] = (102, 255, 255)
+    im.show()
+
+# two standard for black bars
+# the white bar length > white_len = 8
+# the black bar length > black_len = 30
+
+#  select those valid white row bar
+valid_white_bar_rows = []
+for x in white_bar_rows:
+    if(x[2] > white_len): # length of white bar 
+        valid_white_bar_rows.append(x)
+
+black_rows = []
+for x in range(len(valid_white_bar_rows)-1):
+    if(valid_white_bar_rows[x+1][0] - valid_white_bar_rows[x][1] > black_len):
+        black_rows.append((valid_white_bar_rows[x][1]+1, valid_white_bar_rows[x+1][0]-1))
+
+#  select those valid white col bar
+valid_white_bar_cols = []
+for x in white_bar_cols:
+    if(x[2] > white_len): # length of white bar 
+        valid_white_bar_cols.append(x)
+
+black_cols = []
+for x in range(len(valid_white_bar_cols)-1):
+    if(valid_white_bar_cols[x+1][0] - valid_white_bar_cols[x][1] > black_len):
+        black_cols.append((valid_white_bar_cols[x][1]+1, valid_white_bar_cols[x+1][0]-1))
+
+print '*****************row cooridinates***************'
+for x in black_rows:
+    print x
+
+print '*****************col cooridinates***************'
+for x in black_cols:
+    print x
+
+im.show()
+# //////////////////////////////////////////////////////////////////////////////////////
+# /////////////////////END OF SECOND TIME
+# ///////////////////////////////////////////////
+
+n = 0
+for y in black_rows:
+    for x in black_cols:
+        n = n + 1
+        box = (x[0], y[0], x[1], y[1])
+        region = im.crop(box)
+        region.save('%s_%s.png' % (f, n), 'png')
+
+print 'n=', n
+if n != 117:
+    print 'ERROR: UNEXPECTED RESULTS'
