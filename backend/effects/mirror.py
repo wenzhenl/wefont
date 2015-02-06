56a99bfec228de927cf8fac7a96fd13f27a4d6fd
Wenzheng Li <wenzhenl@usc.edu>
2015-02-05 16:44:25 -0800

    Doing funny things with JingLei font

---
+++ backend/effects/mirror.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*-
+__author__ = "Wenzheng Li"
+
+
+import numpy as np
+import sys
+import fontforge
+import cv2
+
+font = fontforge.open(sys.argv[1])
+
+print "Font Name : %s" % font.fontname
+print "Full Name : %s" % font.fullname
+print "Family Name : %s" % font.familyname
+print "Version : %s" % font.version
+print "Copyright : %s" % font.copyright
+
+for g in [glyph.glyphname for glyph in font.glyphs()]:
+    print g
+    old = font[g]
+    new = font.createChar(-1, 'dummytmp')
+
+    pens = []
+    pen = new.glyphPen()
+    for  contour in old.layers[1]:
+        pen.moveTo((contour[0].x, contour[0].y))
+        for point in contour[1:]:
+            if point.on_curve:
+                pen.lineTo((point.x, point.y))
+        pen.closePath()
+        pens.append(pen)
+    old.clear()
+    for pen in pens:
+        old.draw(pen)
+font.removeGlyph("dummytmp")
+font.generate("Lee.ttf")
