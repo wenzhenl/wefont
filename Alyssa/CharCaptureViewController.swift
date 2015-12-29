6ff65bba11823bb06b8a07012d76037f5050abe2
Wenzheng Li <wenzhenl@usc.edu>
2015-12-29 14:52:33 -0800

    try to limit the number of chinese chars, but failed
    Former-commit-id: 24d0c79a2272b2f6890a204d3d0872d0aab33f79

--- Alyssa/CharCaptureViewController.swift
+++ Alyssa/CharCaptureViewController.swift
@@ -27,10 +27,42 @@ class CharCaptureViewController: UIViewController, UITextFieldDelegate {
         
         self.toolbar.tintColor = Settings.ColorOfStamp
         self.currentCharContainerView.backgroundColor = UIColor.clearColor()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "textFieldEditChanged:", name: "UITextFieldTextDidChangeNotification", object: currentCharTextField)
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+    
+//    func textFieldEditChanged(notification : NSNotification) {
+//        let textField = notification.object as! UITextField
+//        let newString = textField.text
+//        let lang = textField.textInputMode?.primaryLanguage
+//        if lang == "zh-Hans" {
+//            let selectedRange = textField.markedTextRange
+//            let position = textField.positionFromPosition((selectedRange?.start)!, inDirection: .Left, offset: 0)
+//            if position == nil {
+//                if newString?.characters.count > 1 {
+//                    let index = newString?.endIndex.advancedBy(-1)
+//                    textField.text = newString?.substringToIndex(index!)
+//                }
+//            }
+//        } else {
+//            if newString?.characters.count > 1 {
+//                let index = newString?.endIndex.advancedBy(-1)
+//                textField.text = newString?.substringToIndex(index!)
+//            }
+//        }
+//    }
+//    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+//        let currentCharacterCount = textField.text?.characters.count ?? 0
+//        print(currentCharacterCount)
+//        if (range.length + range.location > currentCharacterCount){
+//            return false
+//        }
+//        let newLength = currentCharacterCount + string.characters.count - range.length
+//        return newLength <= 1
+//    }
 }
