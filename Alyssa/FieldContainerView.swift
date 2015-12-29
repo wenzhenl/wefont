afd6e88a0cd2292830cd354f0d840ea70f59fcb2
Wenzheng Li <wenzhenl@usc.edu>
2015-12-28 17:01:20 -0800

    Finished half of login page
    Former-commit-id: a6af9ae42a925e9dd15b543ad286b0d879c2254f

---
+++ Alyssa/FieldContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  FieldContainerView.swift
+//  Alyssa
+//
+//  Created by Wenzheng Li on 12/28/15.
+//  Copyright © 2015 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+class FieldContainerView: UIView {
+
+    override func drawRect(rect: CGRect) {
+        let middleLinePath = UIBezierPath()
+        middleLinePath.moveToPoint(CGPoint(x: 0, y: self.bounds.height/2))
+        middleLinePath.addLineToPoint(CGPoint(x: self.bounds.width, y: self.bounds.height/2))
+        UIColor.lightGrayColor().set()
+        middleLinePath.stroke()
+    }
+}
---
+++ Alyssa/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Alyssa
+//
+//  Created by Wenzheng Li on 12/28/15.
+//  Copyright © 2015 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var loginButtonContainerView: UIView!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var textFieldContainerView: UIView!
+    
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            emailTextField.delegate = self
+        }
+    }
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet {
+            passwordTextField.delegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loginButtonContainerView.backgroundColor = Settings.ColorOfStamp
+        loginButtonContainerView.layer.cornerRadius = 4
+        
+        textFieldContainerView.backgroundColor = UIColor.clearColor()
+        textFieldContainerView.layer.cornerRadius = 4
+        textFieldContainerView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        textFieldContainerView.layer.borderWidth = 1
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
